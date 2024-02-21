273b2939ddae6e2c2a737cfe2721aa316f429baf
Jacky Chen <chenjacky32@gmail.com>
2024-02-21 11:08:32 +0700

    feat: fitur fetching data Jokes

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ const Tambah = (a) => {
   }
 };
 console.log(Tambah(200));
+
+// Fetching Data
+
+const Getjokes = async () => {
+  try {
+    const Config = {
+      headers: {
+        Accept: "application/json",
+        //untuk header nya tdk hanya header,tetapi bisa masukkin body,data sesuai parameter axiosnya
+      },
+    };
+    const Res = await axios.get("https://icanhazdadjoke.com/", Config);
+    return Res.data.joke;
+  } catch (error) {
+    return "Jokes not found 404";
+  }
+};
+
+console.log(Getjokes()); //utk liat data didalam api yg kita panggil
